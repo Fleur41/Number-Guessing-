59bf7ec086c963d72d04861635e9cfc44100d73e
Fleur41 <Muriithikings456@gmail.com>
2023-12-12 10:28:43 +0300

    attempts_list

--- Number.py
+++ Number.py
@@ -2,3 +2,16 @@
 Number Guessing Game
 -------------------------------------------------------------
 '''
+
+import random
+
+attempts_list = []
+
+def show_score():
+    if not attempts_list:
+        print('There is currently no high score,'
+              ' it\'s yours for the taking!')
+        
+    else:
+        print(f'The current high score is'
+              f' {min(attempts_list)} attempts')
